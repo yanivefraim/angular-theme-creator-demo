36c296071ae8633aab085d46ef4f31241d59485f
Yaniv Efraim <yaniv.efraim@gmail.com>
2015-08-16 11:19:00 +0300

    cleanup

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,4 @@
  */
 angular.module('themeCreatorDemoApp')
   .controller('MainCtrl', function () {
-    this.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
   });
